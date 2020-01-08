63ddf4b5883a4888e9396fbe0e7a25e9076a49ce
Vinethri <36619780+Vinethri@users.noreply.github.com>
2020-01-08 15:37:31 +0530

    Add files via upload

---
+++ IT/functions.php
@@ -0,0 +1,79 @@
+<?php 
+session_start();
+
+include ('connection.php');
+
+function isLoggedIn()
+{
+	if (isset($_SESSION['email']) && $_SESSION['user_type'] == "IT") {
+		return true;
+	}else{
+		return false;
+	}
+}
+
+// log user out if LOGOUT BUTTON clicked
+if (isset($_GET['logout'])) {
+    // remove all session variables
+	session_unset();
+    // destroy the session
+	session_destroy();
+	header("location: ../index.php");
+}
+
+
+// variable declaration
+$bid = $proposal = "";
+$errors = array(); 
+
+// call the register() function if add_btn is clicked
+if (isset($_POST['submit'])) {
+	placebid();
+}
+
+// POST PROJECTS
+function placebid(){
+	// call these variables with the global keyword to make them available in function
+	global $conn, $errors, $bid, $days, $proposal;
+
+	// receive all input values from the form.
+    // defined below to escape form values
+	
+	$bid =  $_POST['bid'];
+	$days =  $_POST['days'];
+	$proposal =  $_POST['proposal'];
+	
+
+	// form validation: ensure that the form is correctly filled
+
+	if (empty($bid)) { 
+		array_push($errors, "Bid amount is required."); 
+	}
+
+	if (empty($days)) { 
+		array_push($errors, " required."); 
+	}
+
+	if (empty($proposal)) { 
+		array_push($errors, "proposal is required."); 
+	}
+
+	// register user if there are no errors in the form
+	if (count($errors) == 0) {
+		
+		$query = "INSERT INTO bid (Bid, Days, Proposal) 
+				  VALUES('$bid', '$days', '$proposal')";
+		if ($conn->query($query) === TRUE) {
+		    echo "New record created successfully";
+		} else {
+		    echo "Error: " . $query . "<br>" . $conn->error;
+		}
+	}
+	$conn->close();
+}
+
+
+
+
+
+?>
