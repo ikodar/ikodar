f9ce410af312b529f51c230450f7607242ea3736
virasha96 <virasha96amaraweera@gmail.com>
2020-01-09 11:15:17 +0530

    ui tika ohomamai monawa kalath html tiyenewa php kalaama vada nh e nisa aye html dmma

--- client/viewopen.php
+++ client/viewopen.php
@@ -28,7 +28,7 @@
    <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
    <!-- CSS Files -->
-   <link href="../assets/css/material-dashboard.css?v=2.1.1" rel="stylesheet" />
+   <link href="assets/css/material-dashboard.css?v=2.1.1" rel="stylesheet" />
    
  </head>
  
@@ -48,37 +48,37 @@
        <div class="sidebar-wrapper">
          <ul class="nav">
            <li class="nav-item active  ">
-             <a class="nav-link" href="./dashboard.html">
+             <a class="nav-link" href="./home.php">
                <!--<i class="material-icons">dashboard</i>-->
-               <p>Dashboard</p>
+               <p>Home</p>
              </a>
            </li>
            <li class="nav-item active">
-             <a class="nav-link" href="./addprojects.html">
+             <a class="nav-link" href="./addprojects.php">
                
                <p>Add Projects</p>
              </a>
            </li>
            <li class="nav-item active">
-             <a class="nav-link" href="./viewopen.html">
+             <a class="nav-link" href="./viewopen.php">
                <!--<i class="material-icons">content_paste</i>-->
                <p>View Open Projects</p>
              </a>
            </li>
            <li class="nav-item active">
-             <a class="nav-link" href="./viewpast.html">
+             <a class="nav-link" href="./viewpast.php">
                <!--<i class="material-icons">library_books</i>-->
                <p>View Past Project</p>
              </a>
            </li>
            <li class="nav-item active">
-             <a class="nav-link" href="./myprofile.html">
+             <a class="nav-link" href="./myprofile.php">
                <!--<i class="material-icons">bubble_chart</i>-->
                <p>My Profile</p>
              </a>
            </li>
            <li class="nav-item active">
-             <a class="nav-link" href="./payments.html">
+             <a class="nav-link" href="./payments.php">
                <!--<i class="material-icons">location_ons</i>-->
                <p>Payments</p>
              </a>
@@ -186,47 +186,48 @@
        
 
      
-   <!--   Core JS Files   -->
-   <script src="../assets/js/core/jquery.min.js"></script>
-   <script src="../assets/js/core/popper.min.js"></script>
-   <script src="../assets/js/core/bootstrap-material-design.min.js"></script>
-   <script src="../assets/js/plugins/perfect-scrollbar.jquery.min.js"></script>
-   <!-- Plugin for the momentJs  -->
-   <script src="../assets/js/plugins/moment.min.js"></script>
-   <!--  Plugin for Sweet Alert -->
-   <script src="../assets/js/plugins/sweetalert2.js"></script>
-   <!-- Forms Validations Plugin -->
-   <script src="../assets/js/plugins/jquery.validate.min.js"></script>
-   <!-- Plugin for the Wizard, full documentation here: https://github.com/VinceG/twitter-bootstrap-wizard -->
-   <script src="../assets/js/plugins/jquery.bootstrap-wizard.js"></script>
-   <!--	Plugin for Select, full documentation here: http://silviomoreto.github.io/bootstrap-select -->
-   <script src="../assets/js/plugins/bootstrap-selectpicker.js"></script>
-   <!--  Plugin for the DateTimePicker, full documentation here: https://eonasdan.github.io/bootstrap-datetimepicker/ -->
-   <script src="../assets/js/plugins/bootstrap-datetimepicker.min.js"></script>
-   <!--  DataTables.net Plugin, full documentation here: https://datatables.net/  -->
-   <script src="../assets/js/plugins/jquery.dataTables.min.js"></script>
-   <!--	Plugin for Tags, full documentation here: https://github.com/bootstrap-tagsinput/bootstrap-tagsinputs  -->
-   <script src="../assets/js/plugins/bootstrap-tagsinput.js"></script>
-   <!-- Plugin for Fileupload, full documentation here: http://www.jasny.net/bootstrap/javascript/#fileinput -->
-   <script src="../assets/js/plugins/jasny-bootstrap.min.js"></script>
-   <!--  Full Calendar Plugin, full documentation here: https://github.com/fullcalendar/fullcalendar    -->
-   <script src="../assets/js/plugins/fullcalendar.min.js"></script>
-   <!-- Vector Map plugin, full documentation here: http://jvectormap.com/documentation/ -->
-   <script src="../assets/js/plugins/jquery-jvectormap.js"></script>
-   <!--  Plugin for the Sliders, full documentation here: http://refreshless.com/nouislider/ -->
-   <script src="../assets/js/plugins/nouislider.min.js"></script>
-   <!-- Include a polyfill for ES6 Promises (optional) for IE11, UC Browser and Android browser support SweetAlert -->
-   <script src="https://cdnjs.cloudflare.com/ajax/libs/core-js/2.4.1/core.js"></script>
-   <!-- Library for adding dinamically elements -->
-   <script src="../assets/js/plugins/arrive.min.js"></script>
-   <!--  Google Maps Plugin    -->
-   <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"></script>
-   <!-- Chartist JS -->
-   <script src="../assets/js/plugins/chartist.min.js"></script>
-   <!--  Notifications Plugin    -->
-   <script src="../assets/js/plugins/bootstrap-notify.js"></script>
-   <!-- Control Center for Material Dashboard: parallax effects, scripts for the example pages etc -->
-   <script src="../assets/js/material-dashboard.js?v=2.1.1" type="text/javascript"></script>
+  <!--   Core JS Files   -->
+  <script src="assets/js/core/jquery.min.js"></script>
+  <script src="assets/js/core/popper.min.js"></script>
+  <script src="assets/js/core/bootstrap-material-design.min.js"></script>
+  <script src="assets/js/plugins/perfect-scrollbar.jquery.min.js"></script>
+  <!-- Plugin for the momentJs  -->
+  <script src="assets/js/plugins/moment.min.js"></script>
+  <!--  Plugin for Sweet Alert -->
+  <script src="assets/js/plugins/sweetalert2.js"></script>
+  <!-- Forms Validations Plugin -->
+  <script src="assets/js/plugins/jquery.validate.min.js"></script>
+  <!-- Plugin for the Wizard, full documentation here: https://github.com/VinceG/twitter-bootstrap-wizard -->
+  <script src="assets/js/plugins/jquery.bootstrap-wizard.js"></script>
+  <!--	Plugin for Select, full documentation here: http://silviomoreto.github.io/bootstrap-select -->
+  <script src="assets/js/plugins/bootstrap-selectpicker.js"></script>
+  <!--  Plugin for the DateTimePicker, full documentation here: https://eonasdan.github.io/bootstrap-datetimepicker/ -->
+  <script src="assets/js/plugins/bootstrap-datetimepicker.min.js"></script>
+  <!--  DataTables.net Plugin, full documentation here: https://datatables.net/  -->
+  <script src="assets/js/plugins/jquery.dataTables.min.js"></script>
+  <!--	Plugin for Tags, full documentation here: https://github.com/bootstrap-tagsinput/bootstrap-tagsinputs  -->
+  <script src="assets/js/plugins/bootstrap-tagsinput.js"></script>
+  <!-- Plugin for Fileupload, full documentation here: http://www.jasny.net/bootstrap/javascript/#fileinput -->
+  <script src="assets/js/plugins/jasny-bootstrap.min.js"></script>
+  <!--  Full Calendar Plugin, full documentation here: https://github.com/fullcalendar/fullcalendar    -->
+  <script src="assets/js/plugins/fullcalendar.min.js"></script>
+  <!-- Vector Map plugin, full documentation here: http://jvectormap.com/documentation/ -->
+  <script src="assets/js/plugins/jquery-jvectormap.js"></script>
+  <!--  Plugin for the Sliders, full documentation here: http://refreshless.com/nouislider/ -->
+  <script src="assets/js/plugins/nouislider.min.js"></script>
+  <!-- Include a polyfill for ES6 Promises (optional) for IE11, UC Browser and Android browser support SweetAlert -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/core-js/2.4.1/core.js"></script>
+  <!-- Library for adding dinamically elements -->
+  <script src="assets/js/plugins/arrive.min.js"></script>
+  <!--  Google Maps Plugin    -->
+  <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"></script>
+  <!-- Chartist JS -->
+  <script src="assets/js/plugins/chartist.min.js"></script>
+  <!--  Notifications Plugin    -->
+  <script src="assets/js/plugins/bootstrap-notify.js"></script>
+  <!-- Control Center for Material Dashboard: parallax effects, scripts for the example pages etc -->
+  <script src="assets/js/material-dashboard.js?v=2.1.1" type="text/javascript"></script>
+  script>
    
    
  </body>
