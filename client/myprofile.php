8c8cd7ae1cf51ab202e31e69dc1f84d400277120
shelomi123 <shelomipriskila1995@gmail.com>
2020-01-17 16:21:47 +0530

    fff

--- client/myprofile.php
+++ client/myprofile.php
@@ -28,7 +28,7 @@
    <!-- CSS dfdfJust for demo purpose, don't include it in your project -->
    <link href="../assets/demo/demo.css" rel="stylesheet" />
 
-   <!--<link href="css/custom.css" rel="stylesheet" />-->
+   <link href="css/custom.css" rel="stylesheet" />
  </head>
  
  <body class="">
