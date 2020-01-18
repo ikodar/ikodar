a0d0fb71466897967fd0ba50c7fd5c56d5678867
shelomi123 <shelomipriskila1995@gmail.com>
2020-01-18 12:27:58 +0530

    com

--- client/viewMyprofile.php
+++ client/viewMyprofile.php
@@ -1,12 +1,13 @@
-<?php 
-  
-  include('processMyprfl.php');
-	if (!isLoggedIn()) {
-	$_SESSION['msg'] = "You must log in first";
-	header('location: ../login.php');
-  }
+
+ <?php include ('process.php');
+    if (!isLoggedIn()) {
+    $_SESSION['msg'] = "You must update first";
+    header('location: myprofile.php');
+    }
+
+
+ ?>
  
-?>
  <!DOCTYPE html>
  <html lang="en">
  
@@ -16,7 +17,7 @@
    <link rel="icon" type="image/png" href="../assets/img/favicon.png">
    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
    <title>
-     myprofile 
+     ikodar
    </title>
    <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
    <!--     Fonts and icons     -->
@@ -24,11 +25,10 @@
    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
    <!-- CSS Files -->
    <link href="css/material-dashboard.css?v=2.1.1" rel="stylesheet" />
-
-   <!-- CSS dfdfJust for demo purpose, don't include it in your project -->
-   <link href="../assets/demo/demo.css" rel="stylesheet" />
-
    <link href="css/custom.css" rel="stylesheet" />
+   <!-- CSS Just for demo purpose, don't include it in your project -->
+   <link href="../assets/demo/demo.css" rel="stylesheet" />
+   <link href="css/custom.css" rel="stylesheet">
  </head>
  
  <body class="">
@@ -39,35 +39,34 @@
  
          Tip 2: you can also add an image using data-image tag
      -->
-     <div class="logo">
+       <div class="logo">
          <a href="http://www.creative-tim.com" class="simple-text logo-normal">
            ikodar
          </a>
        </div>
-       
        <div class="sidebar-wrapper">
          <ul class="nav">
            <li class="nav-item active  ">
              <a class="nav-link" href="./home.php">
                <!--<i class="material-icons">dashboard</i>-->
-               <p>Dashboard</p>
+               <p>Home</p>
              </a>
-             </li>
-             <li class="nav-item active  ">
-               <a class="nav-link" href="./projects.php">
-                 <!--<i class="material-icons">dashboard</i>-->
-                 <p>My Projects</p>
-               </a>
- 
            </li>
+           <li class="nav-item active">
+             <a class="nav-link" href="./projects.php">
+               
+               <p>My Projects</p>
+             </a>
+           </li>
+           
+           
            <li class="nav-item active">
              <a class="nav-link" href="./myprofile.php">
                <!--<i class="material-icons">bubble_chart</i>-->
                <p>My Profile</p>
              </a>
            </li>
-          </li>
-          <li class="nav-item active">
+           <li class="nav-item active">
             <a class="nav-link" href="./projects.php">
               
               <p>Messages</p>
@@ -79,6 +78,7 @@
                <p>Payments</p>
              </a>
            </li>
+           
          </ul>
        </div>
      </div>
@@ -87,7 +87,7 @@
        <nav class="navbar navbar-expand-lg navbar-transparent navbar-absolute fixed-top ">
          <div class="container-fluid">
            <div class="navbar-wrapper">
-             <a class="navbar-brand" href="#pablo">My Projects</a>
+             <a class="navbar-brand" href="#pablo">View Past Projects</a>
            </div>
            <button class="navbar-toggler" type="button" data-toggle="collapse" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
              <span class="sr-only">Toggle navigation</span>
@@ -108,13 +108,14 @@
              <ul class="navbar-nav">
                <li class="nav-item">
                  <a class="nav-link" href="#pablo">
-                   <!--<i class="material-icons">Dashboard</i>-->
+                   <i class="material-icons">Dashboard</i>
                    <p class="d-lg-none d-md-block">
-                     Dashboard
+                     Stats
                    </p>
                  </a>
                </li>
-
+               <li class="nav-item dropdown">
+                 
                <li class="nav-item">
                  <a class="nav-link" href="contactus.php">Contact Us</a>
                </li>
@@ -131,8 +132,22 @@
            </div>
          </div>
        </nav>
- 
-       <!-- End Navbar -->
+       <!-- End Navbar 
+       <div class="col-lg-3 col-md-6 col-sm-6" >
+               <div class="card card-stats" >
+                 
+                   <div class="card-icon" style="background-color:#00008b">
+                     <i class="material-icons" style="background-color:blue">content_copy</i>
+                   </div>
+                   <p class="card-category" style="font-weight:bold " >New projects</p>
+                   <h3 class="card-title" >5
+                     <small></small>
+                   </h3>
+                </div>
+                
+               </div>
+             </div>
+       -->
        <div class="content" style="width:1500px">
          <div class="container-fluid" style="width:1500px">
            
@@ -142,97 +157,144 @@
                <div class="card">
                  <div class="card-header card-header-tabs card-header-primary">
                    
-                 
-                
-                        
-                                <ul class="nav nav-tabs col-md-8" style="background-color:#113849; padding:20px; margin-left:15%">
+                      <ul class="nav nav-tabs col-md-8" style="background-color:#113849; padding:20px; margin-left:15%">
                                   
                                    
                                     
-                                      <a class="nav-link" href="myprofile.php" style="color:white; font-size:20px; margin-left:5%">Edit Profile</a>
-                                    
-                                    
-                                      <a class="nav-link" href="viewMyprofile.php" style="color:white;  font-size:20px; margin-left:5%">View Profile</a>
-                                  
-                              </ul>
-                  
-                     
-                      
+                          <a class="nav-link" href="myprofile.php" style="color:white; font-size:20px; margin-left:5%">Edit Profile</a>
+                                
+                                
+                          <a class="nav-link" href="viewMyprofile.php" style="color:white;  font-size:20px; margin-left:5%">View Profile</a>
+                              
+                      </ul>
 
                  </div>
                  <div class="card-body">
-                   <form method="post" action="myprofile.php"  >
-                     <div class="row">
-                       <div class="col-md-4">
-                         <div class="form-group">
-                           <label class="bmd-label-floating" >Email address</label>
-                           <input name="email" type="email" class="form-control" style="width:500px" value="<?php echo $_SESSION['email'];?>">
-                         </div>
-                       </div>
-                     </div>
-                     <div class="row">
-                       <div class="col-md-6">
-                         <div class="form-group">
-                           <label class="bmd-label-floating">First Name</label>
-                           <input name="firstname" type="text" class="form-control">
-                         </div>
-                       </div>
-                       <div class="col-md-6">
-                         <div class="form-group">
-                           <label class="bmd-label-floating">Last Name</label>
-                           <input name="lastname" type="text" class="form-control">
-                         </div>
-                       </div>
-                     </div>
-                     <div class="row">
-                       <div class="col-md-12">
-                         <div class="form-group">
-                           <label class="bmd-label-floating">Adress</label>
-                           <input name="address" type="text" class="form-control">
-                         </div>
-                       </div>
-                     </div>
-                     <div class="row">
-                       <div class="col-md-4">
-                         <div class="form-group">
-                           <label class="bmd-label-floating">City</label>
-                           <input name="city" type="text" class="form-control">
-                         </div>
-                       </div>
-                       <div class="col-md-4">
-                         <div class="form-group">
-                           <label class="bmd-label-floating">Country</label>
-                           <input name="country" type="text" class="form-control">
-                         </div>
-                       </div>
-                       <div class="col-md-4">
-                         <div class="form-group">
-                           <label class="bmd-label-floating">Postal Code</label>
-                           <input name="postalcode" type="text" class="form-control">
-                         </div>
-                       </div>
-                     </div>
-                     <div class="row">
-                       <div class="col-md-12">
-                         <div class="form-group">
-                           <label>About Me</label>
-                           <div class="form-group">
+                   <div class="tab-content">
+                     <div class="tab-pane active" id="profile">
+                       <table class="table">
+                         <tbody>
+                           <tr>
                              
-                             <textarea name="about" style="width:520px" class="input" rows="5"></textarea>
-                           </div>
-                         </div>
-                       </div>
+                             <td>Email</td>
+                             <td><input name="email" type="email" class="form-control" style="width:500px" value="<?php echo $_SESSION['email'];?>"></td>
+                             
+                           </tr>
+                           <tr>
+                             
+                             <td>First Name</td>
+                             <td><input name="firstname" type="text" class="form-control" value="<?php echo $_SESSION['firstname'];?>"></td>
+                             <td class="td-actions text-right">
+                               <button type="button" rel="tooltip" title="Edit Task" class="btn btn-primary btn-link btn-sm">
+                                 <i class="material-icons">edit</i>
+                               </button>
+                               <button type="button" rel="tooltip" title="Remove" class="btn btn-danger btn-link btn-sm">
+                                 <i class="material-icons">close</i>
+                               </button>
+                             </td>
+                           </tr>
+                           <tr>
+                             
+                             <td>Last Name</td>
+                             <td><input name="lastname" type="text" class="form-control" value="<?php echo $_SESSION['email'];?>"></td>
+                             <td class="td-actions text-right">
+                               <button type="button" rel="tooltip" title="Edit Task" class="btn btn-primary btn-link btn-sm">
+                                 <i class="material-icons">edit</i>
+                               </button>
+                               <button type="button" rel="tooltip" title="Remove" class="btn btn-danger btn-link btn-sm">
+                                 <i class="material-icons">close</i>
+                               </button>
+                             </td>
+                           </tr>
+                           <tr>
+                             
+                             <td>Address</td>
+                             <td><input name="address" type="text" class="form-control" value="<?php echo $_SESSION['email'];?>"></td>
+                             <td class="td-actions text-right">
+                               <button type="button" rel="tooltip" title="Edit Task" class="btn btn-primary btn-link btn-sm">
+                                 <i class="material-icons">edit</i>
+                               </button>
+                               <button type="button" rel="tooltip" title="Remove" class="btn btn-danger btn-link btn-sm">
+                                 <i class="material-icons">close</i>
+                               </button>
+                             </td>
+                           </tr>
+                        
+                           <tr>
+                             
+                             <td>City</td>
+                             <td><input name="city" type="text" class="form-control" value="<?php echo $_SESSION['email'];?>"></td>
+                             <td class="td-actions text-right">
+                               <button type="button" rel="tooltip" title="Edit Task" class="btn btn-primary btn-link btn-sm">
+                                 <i class="material-icons">edit</i>
+                               </button>
+                               <button type="button" rel="tooltip" title="Remove" class="btn btn-danger btn-link btn-sm">
+                                 <i class="material-icons">close</i>
+                               </button>
+                             </td>
+                           </tr>
+                           <tr>
+                             
+                             <td>Country</td>
+                             <td><input name="country" type="text" class="form-control" value="<?php echo $_SESSION['email'];?>"></td>
+                             <td class="td-actions text-right">
+                               <button type="button" rel="tooltip" title="Edit Task" class="btn btn-primary btn-link btn-sm">
+                                 <i class="material-icons">edit</i>
+                               </button>
+                               <button type="button" rel="tooltip" title="Remove" class="btn btn-danger btn-link btn-sm">
+                                 <i class="material-icons">close</i>
+                               </button>
+                             </td>
+                           </tr>
+                         
+                           <tr>
+                             
+                             <td>Postal Code</td>
+                             <td><input name="postalcode" type="text" class="form-control" value="<?php echo $_SESSION['email'];?>"></td>
+                             <td class="td-actions text-right">
+                               <button type="button" rel="tooltip" title="Edit Task" class="btn btn-primary btn-link btn-sm">
+                                 <i class="material-icons">edit</i>
+                               </button>
+                               <button type="button" rel="tooltip" title="Remove" class="btn btn-danger btn-link btn-sm">
+                                 <i class="material-icons">close</i>
+                               </button>
+                             </td>
+                           </tr>
+                           <tr>
+                             
+                             <td>About me</td>
+                             <td>
+                              <div class="form-group">
+                             
+                                  <textarea name="about" style="width:450px" class="input" rows="5"></textarea>
+                              </div>
+
+                             </td>
+                             <td class="td-actions text-right">
+                               <button type="button" rel="tooltip" title="Edit Task" class="btn btn-primary btn-link btn-sm">
+                                 <i class="material-icons">edit</i>
+                               </button>
+                               <button type="button" rel="tooltip" title="Remove" class="btn btn-danger btn-link btn-sm">
+                                 <i class="material-icons">close</i>
+                               </button>
+                             </td>
+                           </tr>
+                           
+                         </tbody>
+                       </table>
                      </div>
-                     
-                     <input class="btn btn-primary pull-right" type="submit" name="submit_btn" value="Update Profile" href="viewMyprofile.php">
-                     <div class="clearfix"></div>
-                   </form>
+                   </div>
                  </div>
                </div>
              </div>
- 
              
-                         
+           </div>
+         </div>
+       </div>
+       
+     </div>
+   </div>
+  
    <!--   Core JS Files   -->
    <script src="../assets/js/core/jquery.min.js"></script>
    <script src="../assets/js/core/popper.min.js"></script>
@@ -447,6 +509,14 @@
        });
      });
    </script>
+   <script>
+     $(document).ready(function() {
+       // Javascript method's body can be found in assets/js/demos.js
+       md.initDashboardPageCharts();
+ 
+     });
+   </script>
  </body>
  
  </html>
+ 
