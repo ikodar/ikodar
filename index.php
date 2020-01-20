0877a2249d5edd35314208a1451d4612e1bc88f6
Vinethri <36619780+Vinethri@users.noreply.github.com>
2020-01-20 12:23:11 +0530

    index page changes

--- index.php
+++ index.php
@@ -28,18 +28,19 @@
             <h4 style="color: white;">i-කෝඩර්</h4>
         </div>
           <ul class="nav navbar-nav navbar-right">
-            <li><a href="#page-top">Home</a></li>
-            <li><a href="#about">About Us</a></li>
-            <li><a href="#contact">Contact us</a></li>
-            <li><a href="login.php">Sign in</a></li>
-            <li><a href="register.php">Sign up</a></li>
+            <li style="padding-left:20px;"><a href="#page-top">Home</a></li>
+            <li style="padding-left:20px;"><a href="#about">About Us</a></li>
+            <li style="padding-left:20px;"><a href="#projects">Projects</a></li>
+            <li style="padding-left:20px;"><a href="#contact">Contact us</a></li>
+            <li style="padding-left:20px;"><a href="login.php">Sign in</a></li>
+            <li style="padding-left:20px;"><a href="register.php">Sign up</a></li>
           </ul>
       </div>
     </nav>
     <!-- End of navigation bar-->
 
     <div class="container-fluid bg-1 text-center">
-        <h3>i-කෝඩර් We Connect You All</h3>
+        <h3 style="padding-top:50px;">i-කෝඩර් We Connect You All</h3>
         <h4>Welcome To Freelancing and Crowdsourcing !</h4>
     </div>        
    
@@ -74,6 +75,52 @@
         </div>
     </div>
     </section>    
+
+    <section id="projects">
+    <div class="container-fluid bg-5 text-center">
+        <h2>Our Projects</h2>
+        <div class="row text-center">
+          <table class="table">
+              <thead class="thead-dark text-primary">
+                <tr>
+                  <th width="18%">ID</th>
+                  <th width="18%">Name</th>
+                  <th width="18%">Description</th>
+                  <th width="18%">Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                <?php 
+                  //retrieve data from project table
+                  $query = "SELECT * FROM projects";
+                  $results = $conn->query($query);
+                  if ($results->num_rows > 0){
+                    //output data of each row
+                    while ($row = $results->fetch_assoc()) { 
+                        if ($row['status']=="new"){ ?>			
+                          <tr>
+                              <td><?php echo $row['pid']; ?></td>
+                              <td><?php echo $row['name']; ?></td>
+                              <td><?php echo $row['description']; ?></td>
+                              <td>
+                                <div class="input-group">
+                                  <a class="btn btn-primary" href="view.php" role="button" name="view_btn">View</a>
+                                  <a class="btn btn-primary" href="login.php" role="button">Bid</a>
+                                </div>
+                              </td>
+                          </tr>
+                  <?php   }
+                    }
+
+                  }else{
+                    echo "0 results";
+                  }
+                    ?>
+              </tbody>
+          </table>
+        </div>
+    </div>
+    </section> 
 
     <section id="contact">
     <div class="container-fluid bg-3 text-center">
@@ -110,8 +157,8 @@
   </div>
   </section>
 
-    <footer class="container-fluid bg-4 text-center">
-    </footer>
+  <footer class="container-fluid bg-4 text-center">
+  </footer>
 
 </body>
 </html>
