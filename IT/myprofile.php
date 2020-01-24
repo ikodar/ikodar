838cf2bc4be1d6ba26a19793cd1e2500e051c93a
Vinethri <36619780+Vinethri@users.noreply.github.com>
2020-01-24 15:21:16 +0530

    IT logout

--- IT/myprofile.php
+++ IT/myprofile.php
@@ -121,7 +121,7 @@
                  <a class="nav-link" href="help.php">Help</a>
                </li>
                <li class="nav-item"> 
-                 <a class="nav-link" href="../index.php?logout='1'">Logout</a>
+                 <a class="nav-link" href="processMyprfl.php?logout='1'">Logout</a>
                </li>
              </ul>
            </div>
--- IT/myprojects.php
+++ IT/myprojects.php
@@ -120,7 +120,7 @@
                  <a class="nav-link" href="help.php">Help</a>
                </li>
                <li class="nav-item"> 
-                 <a class="nav-link" href="../index.php?logout='1'">Logout</a>
+                 <a class="nav-link" href="functions.php?logout='1'">Logout</a>
                </li>
              </ul>
            </div>
