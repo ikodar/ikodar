eba519341254e887d67319e13617436f8427d96d
Vinethri <36619780+Vinethri@users.noreply.github.com>
2020-02-11 22:45:29 +0530

    admin passwor changes

--- admin/functions.php
+++ admin/functions.php
@@ -27,7 +27,7 @@ if(isset($_POST['view_btn'])){
 	view();
 }
 
-// call the updatepassword() function if save_btn is clicked
+// call the updatepassword() function if update_btn is clicked
 if (isset($_POST['update_btn'])) {
 	updatepassword();
 }
@@ -62,7 +62,52 @@ function saveprofile(){
 	}else{
 		echo "Error: " . $sql . "<br>" . $conn->error;
 	}
-	
-	
+}
+
+//varaibale declaration
+ $passwordErr = $passwordErr1 ="";
+
+//UPDATE the password
+function updatepassword(){
+	//variable declaration
+	global $conn, $passwordErr, $passwordErr1;
+
+	$oldpass = $password1 =$password2= $email ="";
+	$errors = 0;
+
+	$email=$_SESSION['email'];
+	$oldpass = $_POST['oldpass'];
+	$password1 = $_POST['password1'];
+	$password2 = $_POST['password2'];
+
+	// form validation: ensure that the form is correctly filled
+	//checking if the old password is correct
+	if($oldpass != "") {
+		$password = md5($oldpass);
+        $sql = "SELECT password FROM users WHERE email='$email'LIMIT 1";
+        $results=$conn->query($sql);
+        if($results != $password){
+            $passwordErr1="Old password incorrect.";
+            $errors=$errors+1;
+        }
+    }
+
+	//checking if the new password and confirm password match
+	if ($password1 != $password2) {
+        $passwordErr = "The two passwords do not match.";
+        $errors=$errors+1;
+    }
+
+	//update/change the password if there are no errors
+	if ($errors == 0) {
+		$password = md5($password1);
+		$sql = "UPDATE users SET password =$password WHERE email='$email'";
+		if($conn->query($sql) == TRUE){
+			echo "Record updated successfully";
+		}else{
+			echo "Error: " . $sql . "<br>" . $conn->error;
+		}
+	}
+
 }
 ?>
--- admin/profile.php
+++ admin/profile.php
@@ -5,7 +5,7 @@
   header('location: ../login.php');
   
   }
-  
+
   //retrieve current data 
   $email=$_SESSION['email'];
   $sql = "SELECT * FROM users WHERE email='$email'";
@@ -162,7 +162,8 @@
                        <div class="col-md-6">
                          <div class="form-group">
                            <label class="bmd-label-floating">Current Password</label>
-                           <input name="oldpass" type="password" class="form-control">
+                           <input name="oldpass" type="password" class="form-control" required>
+                           <span class="error"><?php echo $passwordErr1;?></span>
                          </div>
                        </div>
                      </div>
@@ -170,7 +171,8 @@
                        <div class="col-md-6">
                          <div class="form-group">
                            <label class="bmd-label-floating">New Password</label>
-                           <input name="password1" type="password" class="form-control">
+                           <input name="password1" type="password" class="form-control" id="psw" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" 
+                            title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters." required>
                          </div>
                        </div>
                      </div>
@@ -178,7 +180,8 @@
                        <div class="col-md-6">
                          <div class="form-group">
                            <label class="bmd-label-floating">Confirm Password</label>
-                           <input name="password2" type="password" class="form-control">
+                           <input name="password2" type="password" class="form-control" required>
+                           <span class="error"><?php echo $passwordErr;?></span>
                          </div>
                        </div>
                      </div>
@@ -190,7 +193,68 @@
                </div>
              </div>
              </div>
-             
+
+             <script>
+    var myInput = document.getElementById("psw");
+    var letter = document.getElementById("letter");
+    var capital = document.getElementById("capital");
+    var number = document.getElementById("number");
+    var length = document.getElementById("length");
+
+    // When the user clicks on the password field, show the message box
+    myInput.onfocus = function() {
+      document.getElementById("message").style.display = "block";
+    }
+
+    // When the user clicks outside of the password field, hide the message box
+    myInput.onblur = function() {
+      document.getElementById("message").style.display = "none";
+    }
+
+    // When the user starts to type something inside the password field
+    myInput.onkeyup = function() {
+      // Validate lowercase letters
+      var lowerCaseLetters = /[a-z]/g;
+      if(myInput.value.match(lowerCaseLetters)) {
+        letter.classList.remove("invalid");
+        letter.classList.add("valid");
+      } else {
+        letter.classList.remove("valid");
+        letter.classList.add("invalid");
+    }
+
+      // Validate capital letters
+      var upperCaseLetters = /[A-Z]/g;
+      if(myInput.value.match(upperCaseLetters)) {
+        capital.classList.remove("invalid");
+        capital.classList.add("valid");
+      } else {
+        capital.classList.remove("valid");
+        capital.classList.add("invalid");
+      }
+
+      // Validate numbers
+      var numbers = /[0-9]/g;
+      if(myInput.value.match(numbers)) {
+        number.classList.remove("invalid");
+        number.classList.add("valid");
+      } else {
+        number.classList.remove("valid");
+        number.classList.add("invalid");
+      }
+
+      // Validate length
+      if(myInput.value.length >= 8) {
+        length.classList.remove("invalid");
+        length.classList.add("valid");
+      } else {
+        length.classList.remove("valid");
+        length.classList.add("invalid");
+      }
+    }
+  </script>
+
+
    <!--   Core JS Files   -->
    <script src="../assets/js/core/jquery.min.js"></script>
    <script src="../assets/js/core/popper.min.js"></script>
