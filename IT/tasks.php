651d45c05531388ec38745b174de09ee4382dad6
Zainfathi <fathimazainab53@gmail.com>
2020-02-16 10:39:14 +0530

    IT

--- IT/tasks.php
+++ IT/tasks.php
@@ -19,112 +19,109 @@
 
 <!DOCTYPE html>
 <html lang="en">
-  <head>
+<head>
   <meta charset="utf-8" />
-    <title>
-      ikodar
-    </title>
-<!--     Fonts and icons     -->
-<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
-<!-- CSS Files -->
-<link href="css/material-dashboard.css?v=2.1.1" rel="stylesheet" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+  <title>Project Details</title>
+  <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
+  <!-- Fonts and icons -->
+  <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
+  <!-- CSS Files -->
+  <link href="css/material-dashboard.css?v=2.1.1" rel="stylesheet" />
   <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   <link href="css/custom.css" rel="stylesheet" />
+
 </head>
 <body class="">
-<div class="wrapper ">
-<div class="sidebar" data-color="azure" data-background-color="white" data-image="../assets/img/sidebar-1.jpg">
- <div class="logo">
- <a class="simple-text logo-normal">
+  <div class="wrapper ">
+    <!--Dashboard panel-->
+    <div class="sidebar" data-color="azure" data-background-color="white" data-image="../assets/img/sidebar-1.jpg">
+      <!--Tip 1: You can change the color of the sidebar using: data-color="purple | azure | green | orange | danger"
+        Tip 2: you can also add an image using data-image tag-->
+      <div class="logo">
+        <a class="simple-text logo-normal">
         i-කෝඩර්
         </a>
         <a class="simple-text logo-normal"><?php echo $_SESSION['email'];?></a>
- </div>
- <div class="sidebar-wrapper">
-   <ul class="nav">
-     <li class="nav-item active  ">
-       <a class="nav-link" href="./home.php">
-         <p>Home</p>
-       </a>
-     </li>
-
-     <li class="nav-item active">
-       <a class="nav-link" href="./projects.php">         
-         <p>My Projects</p>
-       </a>
-     </li>
-     
-     <li class="nav-item active">
-       <a class="nav-link" href="./profile.php">
-         <p>My Profile</p>
-       </a>
-     </li>
-
-     <li class="nav-item active">
-       <a class="nav-link" href="./payments.php">
-         <p>Payments</p>
-       </a>
-     </li>    
-   </ul>
- </div>
-</div>
-<div class="main-panel">
- <!-- Navbar -->
- <nav class="navbar navbar-expand-lg navbar-transparent navbar-absolute fixed-top ">
-   <div class="container-fluid">
-     <div class="navbar-wrapper">
-       <a class="navbar-brand" href="#pablo">
-       <?php 
-                  
-                   $query = "SELECT * FROM projects where pid='$pid'";
-                   $results = $conn->query($query);
-                   if ($results->num_rows > 0) {
-                   //output data of each row
-                   while ($row = $results->fetch_assoc()) { 
-                    { ?>			
-                         <tr>
-                             <td><?php echo $row['name']; ?></a></td>
-                             
-                             
-                         </tr>
-                     <?php   }
-                       }
-     
-                         }else{
-                       echo "0 results";
-                       }
-                     ?>
-     </div>
-     <button class="navbar-toggler" type="button" data-toggle="collapse" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
-       <span class="sr-only">Toggle navigation</span>
-       <span class="navbar-toggler-icon icon-bar"></span>
-       <span class="navbar-toggler-icon icon-bar"></span>
-       <span class="navbar-toggler-icon icon-bar"></span>
-     </button>
-     <div class="collapse navbar-collapse justify-content-end">
-       <form class="navbar-form">        
-       </form>
-       <ul class="navbar-nav">         
-         <li class="nav-item dropdown">        
-            <li class="nav-item">
-              <a class="nav-link" href="aboutus.php">About Us</a>
-            </li>
-
-            <li class="nav-item">
-              <a class="nav-link" href="help.php">Help</a>
-            </li>
-         
-            <li class="nav-item"> 
-              <a class="nav-link" href="process.php?logout='1'">Logout</a>
-            </li>
-          </li>
-        </ul>
       </div>
-    </div>
-  </nav>
- <!-- End Navbar -->
+      <div class="sidebar-wrapper">
+         <ul class="nav">
+           <li class="nav-item active  ">
+             <a class="nav-link" href="./home.php">
+               <!--<i class="material-icons">dashboard</i>-->
+               <p>Dashboard</p>
+             </a>
+ 
+             <li class="nav-item active  ">
+               <a class="nav-link" href="./active.php">
+                 <!--<i class="material-icons">dashboard</i>-->
+                 <p>My Projects</p>
+               </a>
+ 
+           </li>
+           <li class="nav-item active">
+             <a class="nav-link" href="./myprofile.php">
+               <!--<i class="material-icons">bubble_chart</i>-->
+               <p>My Profile</p>
+             </a>
+           </li>
+          </li>
+           <li class="nav-item active">
+             <a class="nav-link" href="./income.php">
+               <!--<i class="material-icons">location_ons</i>-->
+               <p>Income</p>
+             </a>
+           </li>
+         </ul>
+       </div>
+     </div>
+     
+     <div class="main-panel">
+       <!-- Navbar -->
+       <nav class="navbar navbar-expand-lg navbar-transparent navbar-absolute fixed-top ">
+         <div class="container-fluid">
+           <div class="navbar-wrapper">
+             <a class="navbar-brand" href="#pablo">My Projects</a>
+           </div>
+           <button class="navbar-toggler" type="button" data-toggle="collapse" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
+             <span class="sr-only">Toggle navigation</span>
+             <span class="navbar-toggler-icon icon-bar"></span>
+             <span class="navbar-toggler-icon icon-bar"></span>
+             <span class="navbar-toggler-icon icon-bar"></span>
+           </button>
+           <div class="collapse navbar-collapse justify-content-end">
+             
+             <ul class="navbar-nav">
+               <li class="nav-item">
+                 <a class="nav-link" href="#pablo">
+                   <!--<i class="material-icons">Dashboard</i>-->
+                   <p class="d-lg-none d-md-block">
+                     Dashboard
+                   </p>
+                 </a>
+               </li>
+
+               <li class="nav-item">
+                 <a class="nav-link" href="contactus.php">Contact Us</a>
+               </li>
+               <li class="nav-item">
+                 <a class="nav-link" href="aboutus.php">About Us</a>
+               </li>
+               <li class="nav-item">
+                 <a class="nav-link" href="help.php">Help</a>
+               </li>
+               <li class="nav-item"> 
+                 <a class="nav-link" href="functions.php?logout='1'">Logout</a>
+               </li>
+             </ul>
+           </div>
+         </div>
+       </nav>
+ 
+      <!-- End Navbar -->
+
       <div class="content">
         <div class="container-fluid">
           <div class="card-body">
@@ -182,9 +179,10 @@
                           <form action =""	method="post" class="was-validated">
                           
                           <tr>
-                              <input type="hidden" name="tid" value="<?php echo $tid?>"> 
+                              <input type="hidden" name="tid" value="<?php echo $row['tid']?>"> 
                               <td><?php echo $row['task']; ?></td>
                               <td><?php echo $row['hour']; ?></td>
+
                               
  
                               <td style="width:350px;"> 
